14fe605db519eea6e79e604617480707dacbd56f
Minh Hoang <minh.hn2016@gmail.com>
2021-06-08 12:15:30 +0300

    added test for list and create endpoints, run test during container's build

--- backend/src/test/controllers/HomeControllerTest.java
+++ backend/src/test/controllers/HomeControllerTest.java
@@ -6,27 +6,55 @@ import play.inject.guice.GuiceApplicationBuilder;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.WithApplication;
+import play.libs.Json;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import static org.junit.Assert.assertEquals;
 import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.GET;
+import static play.test.Helpers.*;
 import static play.test.Helpers.route;
+import static org.junit.Assert.assertTrue;
 
 public class HomeControllerTest extends WithApplication {
 
-    @Override
-    protected Application provideApplication() {
-        return new GuiceApplicationBuilder().build();
-    }
+	@Override
+	protected Application provideApplication() {
+		return new GuiceApplicationBuilder().build();
+	}
 
-    @Test
-    public void testIndex() {
-        Http.RequestBuilder request = new Http.RequestBuilder()
-                .method(GET)
-                .uri("/");
+	@Test
+	public void testCreatePropertyMethod() {
+		final ObjectNode jsonNode = Json.newObject();
+		jsonNode.put("propertyName", "Test property");
+		jsonNode.put("description", "Test");
+		jsonNode.put("street", "Test street");
+		jsonNode.put("number", 12);
+		jsonNode.put("postalCode", "T35TC0D3");
+		jsonNode.put("city", "TestCity");
+		jsonNode.put("municipality", "Unknown");
+		jsonNode.put("country", "Test Country Rep.");
 
-        Result result = route(app, request);
-        assertEquals(OK, result.status());
-    }
+		Http.RequestBuilder request = new Http.RequestBuilder()
+		.method(POST)
+		.bodyJson(jsonNode)
+		.uri("/properties");
 
+		Result result = route(app, request);
+		assertEquals(CREATED, result.status());
+		assertTrue(result.contentType().isPresent());
+		assertEquals("application/json", result.contentType().get());
+	}
+
+	@Test
+	public void testListingMethod() {
+		Http.RequestBuilder request = new Http.RequestBuilder()
+			.method(GET)
+			.uri("/properties");
+
+		Result result = route(app, request);
+		assertEquals(OK, result.status());
+		assertTrue(result.contentType().isPresent());
+		assertEquals("application/json", result.contentType().get());
+	}
 }
